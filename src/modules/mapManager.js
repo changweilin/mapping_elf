26566f819854b0935599c9fdd4d03abea9419f00
Willy Chang <x111281@gmail.com>
2026-04-23 11:03:39 +0800

    feat: implement waypoint insertion on track click and suppress waypoint creation on double-click

--- src/modules/mapManager.js
+++ src/modules/mapManager.js
@@ -77,9 +77,25 @@ export class MapManager {
     }
     this.tileLayers.topo.addTo(this.map);
 
+    let mapClickTimeout = null;
     this.map.on('click', (e) => {
       if (this.ignoreMapClick) return;
-      this.addWaypoint(e.latlng.lat, e.latlng.lng);
+      if (mapClickTimeout) {
+        clearTimeout(mapClickTimeout);
+        mapClickTimeout = null;
+      } else {
+        mapClickTimeout = setTimeout(() => {
+          mapClickTimeout = null;
+          this.addWaypoint(e.latlng.lat, e.latlng.lng);
+        }, 300);
+      }
+    });
+
+    this.map.on('dblclick', (e) => {
+      if (mapClickTimeout) {
+        clearTimeout(mapClickTimeout);
+        mapClickTimeout = null;
+      }
     });
   }
 
@@ -123,6 +139,25 @@ export class MapManager {
     this._dragWpIndex = undefined;
   }
 
+  _findInsertionIndex(latlng) {
+    if (this.waypoints.length < 2) return this.waypoints.length;
+    let bestIndex = 1;
+    let minDetour = Infinity;
+    for (let i = 1; i < this.waypoints.length; i++) {
+        const w1 = this.waypoints[i - 1];
+        const w2 = this.waypoints[i];
+        const d1 = L.latLng(w1[0], w1[1]).distanceTo(latlng);
+        const d2 = L.latLng(w2[0], w2[1]).distanceTo(latlng);
+        const d12 = L.latLng(w1[0], w1[1]).distanceTo(L.latLng(w2[0], w2[1]));
+        const detour = d1 + d2 - d12;
+        if (detour < minDetour) {
+            minDetour = detour;
+            bestIndex = i;
+        }
+    }
+    return bestIndex;
+  }
+
   switchLayer(layerName) {
     if (!this.tileLayers[layerName]) return;
     this.map.removeLayer(this.tileLayers[this.currentLayerName]);
@@ -130,9 +165,13 @@ export class MapManager {
     this.currentLayerName = layerName;
   }
 
-  addWaypoint(lat, lng) {
-    this.waypoints.push([lat, lng]);
-    const icon = this._createIcon(this.waypoints.length - 1);
+  addWaypoint(lat, lng, insertIndex = null) {
+    const idx = insertIndex !== null ? insertIndex : this.waypoints.length;
+    this.waypoints.splice(idx, 0, [lat, lng]);
+    this.waypointWeather.splice(idx, 0, null);
+    this.waypointColors.splice(idx, 0, null);
+    this.waypointLabels.splice(idx, 0, null);
+    const icon = this._createIcon(idx);
 
     const marker = L.marker([lat, lng], {
       icon,
@@ -345,7 +384,7 @@ export class MapManager {
       this.onWaypointChange(this.waypoints);
     });
 
-    this.waypointMarkers.push(marker);
+    this.waypointMarkers.splice(idx, 0, marker);
     this._updateMarkerIcons();
     this.onWaypointChange(this.waypoints);
   }
@@ -480,10 +519,27 @@ export class MapManager {
         className: `route-line route-${route.index}`,
       }).addTo(this.map);
 
+      let altClickTimeout = null;
       pl._routeIndex = route.index;
       pl.on('click', (e) => {
         L.DomEvent.stopPropagation(e);
-        this.selectRoute(routes, route.index);
+        if (altClickTimeout) {
+          clearTimeout(altClickTimeout);
+          altClickTimeout = null;
+        } else {
+          altClickTimeout = setTimeout(() => {
+            altClickTimeout = null;
+            this.selectRoute(routes, route.index);
+          }, 300);
+        }
+      });
+      pl.on('dblclick', (e) => {
+        L.DomEvent.stopPropagation(e);
+        if (altClickTimeout) {
+          clearTimeout(altClickTimeout);
+          altClickTimeout = null;
+        }
+        pl.bringToFront();
       });
       pl.bindTooltip(route.label, {
         sticky: true,
@@ -550,6 +606,7 @@ export class MapManager {
           lineJoin: 'round',
         }).addTo(this.map);
         this._bindRouteHoverEvents(pl1);
+        this._bindGradientRouteEvents(pl1);
         this.gradientPolylines.push(pl1);
 
         const d2 = dists[splitIdx];
@@ -563,6 +620,7 @@ export class MapManager {
           lineJoin: 'round',
         }).addTo(this.map);
         this._bindRouteHoverEvents(pl2);
+        this._bindGradientRouteEvents(pl2);
         this.gradientPolylines.push(pl2);
         continue;
       }
@@ -592,6 +650,7 @@ export class MapManager {
         lineJoin: 'round',
       }).addTo(this.map);
       this._bindRouteHoverEvents(pl);
+      this._bindGradientRouteEvents(pl);
       this.gradientPolylines.push(pl);
     }
   }
@@ -803,6 +862,32 @@ export class MapManager {
     });
     polyline.on('mouseout', () => {
       if (this.onRouteHover) this.onRouteHover(null, null);
+    });
+  }
+
+  _bindGradientRouteEvents(polyline) {
+    let clickTimeout = null;
+    polyline.on('click', (e) => {
+      L.DomEvent.stopPropagation(e);
+      if (clickTimeout) {
+        clearTimeout(clickTimeout);
+        clickTimeout = null;
+      } else {
+        clickTimeout = setTimeout(() => {
+          clickTimeout = null;
+          const insertIdx = this._findInsertionIndex(e.latlng);
+          this.addWaypoint(e.latlng.lat, e.latlng.lng, insertIdx);
+        }, 300);
+      }
+    });
+
+    polyline.on('dblclick', (e) => {
+      L.DomEvent.stopPropagation(e);
+      if (clickTimeout) {
+        clearTimeout(clickTimeout);
+        clickTimeout = null;
+      }
+      this.gradientPolylines.forEach(gpl => gpl.bringToFront());
     });
   }
 
